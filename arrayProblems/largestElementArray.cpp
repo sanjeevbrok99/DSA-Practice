996bb2daa81c5340625ccb769ef95d093d716586
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-16 22:59:29 +0530

    using lenear search IMLENTATION of largest element of array done

--- arrayProblems/largestElementArray.cpp
+++ arrayProblems/largestElementArray.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using namespace std;
+// Given an array arr of size N, the task is to find the largest element in the given array. 
 
+// Example: 
+
+// Input: arr[] = {10, 20, 4}
+// Output: 20
+
+// Input : arr[] = {20, 10, 20, 4, 100}
+// Output : 100
+
+// solution: 2
 int getlargest(int arr[], int n)
 {
     for (int i = 0; i < n; ++i) {
@@ -27,3 +36,25 @@ int main()
          << getlargest(arr, 4);
     return 0;
 }
+
+//#solution:2
+int largestElement(int arr[],int n){
+    int i;
+    int largest = arr[0];
+    for (i=0;i<n;i++){
+        if(arr[i]>largest)
+            largest = arr[i];
+        
+        
+    }
+
+return largest;
+}
+ int main(){
+    int n;
+    int arr[] = {46,67,5,4,44};
+    n = sizeof(arr)/sizeof(arr[0]);
+    cout << largestElement(arr,n);
+
+    
+ }
