0f4c244e126c0d383b56ceb92ae02d34595d0b4d
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-17 00:45:43 +0530

    second approach done on array is sorted or not

--- arrayProblems/isArraySorted.cpp
+++ arrayProblems/isArraySorted.cpp
@@ -1,22 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isSorted(int arr[], int n){
-    int i,j;
-    for(i=0;i<n;i++){
-        for(j=i+1;j<n;j++){
-            if(arr[j]<arr[i])
-            return false;
-        }
+// bool isSorted(int arr[], int n){
+//     int i,j;
+//     for(i=0;i<n;i++){
+//         for(j=i+1;j<n;j++){
+//             if(arr[j]<arr[i])
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+// int main(){
+//   int arr[]= {1,2,3,4,5,9,7,8};
+//   int n = sizeof(arr)/sizeof(arr[0]);
+//   if(isSorted(arr,n)==true){
+//     cout<< "Yes Array is sorted"<<endl;
+//   }
+//   else
+//   cout<< "Array is not sorted"<<endl;
+// }
+
+// solution 2
+
+bool isArraySorted(int arr[],int n){
+    int i;
+    if(n==0||n == 1){
+        cout<<"Array is sorted"<<endl;
     }
+    for(i = 1; i < n; i++)
+        if(arr[i-1]>arr[i])
+        return false;
     return true;
 }
 int main(){
-  int arr[]= {1,2,3,4,5,9,7,8};
-  int n = sizeof(arr)/sizeof(arr[0]);
-  if(isSorted(arr,n)==true){
-    cout<< "Yes Array is sorted"<<endl;
-  }
-  else
-  cout<< "Array is not sorted"<<endl;
+    int arr[] = {1,2,3,4,8,6,7,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    if(isArraySorted(arr,n)==true)
+    cout<< "Array is sorted"<<endl;
+    else
+    cout<< "Array is not sorted"<<endl;
+
 }
