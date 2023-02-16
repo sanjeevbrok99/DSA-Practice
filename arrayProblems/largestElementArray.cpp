c036cbef28ac5a521810e7d8c3af6c7932007771
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-16 22:39:31 +0530

    function for find greatest element in a array done

---
+++ arrayProblems/largestElementArray.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int getlargest(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i) {
+        bool flag = true;
+        for (int j = i; j < n; ++j) {
+            if (arr[j] > arr[i]) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag == true) {
+            return arr[i];
+        }
+    }
+
+    return -1;
+}
+int main()
+{
+    int arr[] = { 5, 8, 20, 15 };
+    cout << "Largest in given array is "
+         << getlargest(arr, 4);
+    return 0;
+}
