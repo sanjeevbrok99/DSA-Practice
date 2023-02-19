4fa01ca56ba576b374f9452c5ed290c342c3ce15
Sanjeev Mishra <mishragsanjeev1999@gmail.com>
2023-02-19 17:46:10 +0530

    solution for grt maximum diffrence in an array done

--- arrayProblems/maximumDiffrence.cpp
+++ arrayProblems/maximumDiffrence.cpp
@@ -1,3 +1,11 @@
+// Given an array arr[] of integers, find out the maximum difference between any two elements such that larger element appears after the smaller number. 
+
+// Examples : 
+
+// Input : arr = {2, 3, 10, 6, 4, 8, 1}
+// Output : 8
+// Explanation : The maximum difference is between 10 and 2.
+
 #include<bits/stdc++.h>
 using namespace std;
 
