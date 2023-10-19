642a93a2c7c087687e46826d116e45f068fc393a
sanjeevbrok99 <mishragsanjeev1999@gmail.com>
2023-10-19 12:18:13 +0530

    code for peak element updated

--- peakElement.js
+++ peakElement.js
@@ -13,13 +13,21 @@
 // Output: 20 or 90
 // Explanation: The element 20 has neighbors 10 and 15, both of them are less than 20, similarly 90 has neighbors 23 and 67.
 const newArr = [10, 20, 15, 2, 23, 90, 67]
+let n = newArr.length
 
-const isPeak = (arr) =>{
-    for(let i =0;i<arr.length;i++){
+const isPeak = (arr,n) =>{
+    if(arr[0]>arr[1])
+    return 0;
+    if (arr[n-1]>arr[n-2])
+    return n-1
+
+    for(let i=1;i<n;i++){
+        
+
         if(arr[i-1]<arr[i]&& arr[i+1]<arr[i]){
              console.log("peak element is :",arr[i])
         }
     }
 }
 
-isPeak(newArr)
+isPeak(newArr,n)
