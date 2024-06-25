0aa7367a36bb882bbe00fc2efddef2386820e1af
sanjeevbrok99 <mishragsanjeev1999@gmail.com>
2024-06-25 21:42:59 +0530

    left rotate array by d position alogrimth

--- leftRotateArray.js
+++ leftRotateArray.js
@@ -10,18 +10,54 @@
 // Output: 5 6 7 1 2 3 4
 
 
-const rotateArr = (arr,d)=>{
+// const rotateArr = (arr, d) => {
+//     const n = arr.length;
+//     const newArr = new Array(n);
+    
+//     for (let i = 0; i < n - d; i++) {
+//         newArr[i] = arr[d + i];
+//     }
+    
+//     for (let i = 0; i < d; i++) {
+//         newArr[n - d + i] = arr[i];
+//     }
+    
+//     console.log(newArr);
+// }
 
-    const newArr = []
-    for(let i=d;i<arr.length;i++){
-        newArr.push(arr[i])
+// Example usage:
+
+// const rotateArr =(arr,d) => {
+//     const n= arr.length;
+//     const newArr = new Array(n);
+//     for(let i = 0; i < n-d; i++) {
+//         newArr[i]= arr[d+i] }
+
+//     for (let i = 0; i<d; i++) {
+//         newArr[n-d+i]= arr[i];
+//     }
+//          console.log(newArr);
+
+// }
+
+
+const rotateArr = (arr,d)=>{
+    const n = arr.length;
+
+   const rebverseArr = (arr, start , end)=>{
+    while(start<end){
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start++
+        end--
     }
-    for(let i=0;i<d;i++){
-        newArr.push(arr[i])
-    }
-    return console.log(newArr)
+    return arr
+   }
+    rebverseArr(arr,0,n-1)
+    rebverseArr(arr,0,n-d-1)
+    rebverseArr(arr,n-d,n-1)
+   console.log(arr)
 }
 
-let array = [3, 4, 5, 6, 7, 1, 2]
-let d = 2
-rotateArr(array,d)
+rotateArr([1, 2, 3, 4, 5], 2); // Output: [3, 4, 5, 1, 2]
